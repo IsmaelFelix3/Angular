1e19b3262f9c5e8569a43336676427b559902bfe
Ismael Felix <ismaelfelixfavela@gmail.com>
2021-07-28 16:24:21 -0600

    Clase donde se utilizzo el switch map

--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+// el switch map es uno de los operadores de transformacion que te permite recibir un observable
+// y regresar otro observable
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-ver-pais',
@@ -19,17 +22,36 @@ export class VerPaisComponent implements OnInit {
   ngOnInit(): void {
     // cuando el componente esta inicializado
 
-    // el activated route viene con todo lo necesario para subscribirnos a cualquier cambio del url y lo hacemos asi
-    // se podria hacer asi o podriamos usar la desestructuracion
+    // aqui tenemos acceso al observableen el cual estan los parametros
+    // me suscribi al observable params
     this.activatedRoute.params
-    .subscribe( /*params*/  ({id}) =>{
-      console.log(id);
-      // y ahora tengo que subscribirme de nuevo para obtener esta info
-      this.paisService.getPaisPorAlpha(id)
-      .subscribe(pais => {
-        console.log(pais);
-      })
-    })
+    //en el pipe puedo yo puedo especificar cualquier cantidad de operadores que van a trabajar con el producto 
+    // de este observable  this.activatedRoute.params
+    .pipe(
+      // esto tiene que retornar un observable, voy a poner el return implicito por eso no uso llaves
+      // el swith map lo que va a hacer es recibir el valor del observable anterior (params) y
+      // debe de retornar un nuevo observable entonces esto hace ese switch
+      // en lugar de retornar el observable this.activatedRoute.params va a retornar este 
+      // this.paisService.getPaisPorAlpha(params.id) y por eso el resp no se cambia ya que ya es la respuesta del segundo observavle
+      //ahora la repsuesta es el objeto porque ese es el producto de este observable 
+      // this.paisService.getPaisPorAlpha(params.id)
+      switchMap((params) => this.paisService.getPaisPorAlpha(params.id) )
+    )
+    .subscribe(resp => {
+      console.log(resp);
+    });
+
+    // el activated route viene con todo lo necesario para subscribirnos a cualquier cambio del url y lo hacemos asi
+    // // se podria hacer asi o podriamos usar la desestructuracion
+    // this.activatedRoute.params
+    // .subscribe( /*params*/  ({id}) =>{
+    //   console.log(id);
+    //   // y ahora tengo que subscribirme de nuevo para obtener esta info
+    //   this.paisService.getPaisPorAlpha(id)
+    //   .subscribe(pais => {
+    //     console.log(pais);
+    //   })
+    // })
     //de donde se le agrego el id, nosotros lo definimos en el archivo routing ('pais/:id')
     // el nombre que le pongamos despues del slash en el routing es como lo vamos a recibir en los params 
   }
